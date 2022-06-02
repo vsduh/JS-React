f93b30105187f6b36d1d88645f3196c2e1270609
Vyacheslav <ya@live.ru>
2022-06-02 12:02:13 +0300

    The First Push. (less 14)

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src='https://d2dgtayfmpkokn.cloudfront.net/wp-content/uploads/sites/652/2018/07/29163804/proud-bni-member.jpg'></img>
+    </header>
+}
+
+export default Header;
