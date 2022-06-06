b23fc3d968b2e6b18ce5ef9d808e502552db20cc
Vyacheslav <ya@live.ru>
2022-06-06 17:52:43 +0300

    The Second Push. (less 15)

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = () => {
+    return (
+
+                <div className={s.item}>
+                    <img src="https://i.pinimg.com/736x/e1/13/f6/e113f64f714bcf8a32d0b183727e8f38--avatar-film-avatar-theme.jpg" />
+                        Post 1
+                    <div>
+                    <span>like</span>
+                    </div>
+                </div>
+    )
+
+}
+export default Post;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img src='https://cstor.nn2.ru/users/users/foto/188456-2016-03-20-11.jpg.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts/>
+        </div>
+    )
+}
+export default Profile;
