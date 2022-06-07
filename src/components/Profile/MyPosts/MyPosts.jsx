c97a323d13d6843f3bbb1967e30314be96291964
Vyacheslav <ya@live.ru>
2022-06-07 22:00:01 +0300

    The Third Push. (less 16)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,8 +11,8 @@ const MyPosts = () => {
             <button>Remove</button>
 
             <div className={s.posts}>
-                <Post/>
-                <Post/>
+                <Post message='Hi, how are you?'/>
+                <Post message="It's my first post"/>
                 <Post/>
                 <Post/>
                 <Post/>
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
 
                 <div className={s.item}>
                     <img src="https://i.pinimg.com/736x/e1/13/f6/e113f64f714bcf8a32d0b183727e8f38--avatar-film-avatar-theme.jpg" />
-                        Post 1
+                    {props.message }
                     <div>
                     <span>like</span>
                     </div>
