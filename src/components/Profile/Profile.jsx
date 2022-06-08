7537d4c6e25999f1bebc6de7ad9ccee62f4ca1f9
VyacheslavDukhanin <ya@live.ru>
2022-06-08 10:30:50 +0300

    less 18 add component 'Dialogs'

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://cstor.nn2.ru/users/users/foto/188456-2016-03-20-11.jpg.jpg'/>
             </div>
