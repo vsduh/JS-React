1623ff6868503ba041988b7fd0ace8e3fe8c38c9
VyacheslavDukhanin <ya@live.ru>
2022-07-12 09:47:35 +0300

    12.07.22

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 
+
 const App = () => {
     return (
+
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div class='app-wrapper-content'>
-                <Dialogs/>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/dialogs" component={Dialogs}/>
+                        <Route path="/profile" component={Profile}/>
+                    </Routes>
+                </BrowserRouter>
             </div>
-            {/*<Profile />*/}
         </div>
-    );
+
+    )
 }
 
 export default App;
